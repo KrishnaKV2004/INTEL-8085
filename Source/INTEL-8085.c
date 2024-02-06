c02bb5b5a8a4317c91eacf21b1b9d35b1a018fec
Krishna Verma <krishnaverma2004kvaudi@gmail.com>
2024-02-06 22:54:25 +0530

    Headers And Resources

---
+++ Source/INTEL-8085.c
@@ -0,0 +1,20 @@
+//  INTEL-8085 ---->
+
+//  Header Fies ---->
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+//  Local Headers ---->
+
+#include "../Headers/INS_SET.h"
+
+//  Main Function ---->
+
+int main()
+{
+
+
+    return 0;
+}
